bd6a6153624d5919bc1333e4fd6aa994b05b5a8d
jchabrny <87894970+jchabrny@users.noreply.github.com>
2021-10-08 21:30:10 +0200

    Added "s" to "http" in apiUrl

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "04a98ae7fdb3f2cafdd7baf39408fddb";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
